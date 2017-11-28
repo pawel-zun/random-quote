b4787d496b38566cb4f2a42a4b86d0c931b46e2c
Pawel Zun <pzun90@gmail.com>
2017-11-28 21:29:14 +0100

    sample commit text

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var prefix = "https://cors-anywhere.herokuapp.com/";
+// var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
-	$.getJSON(prefix + quoteUrl, createTweet);
+	$.getJSON(/* prefix + */quoteUrl, createTweet);
 }
 
 function createTweet(input) {
